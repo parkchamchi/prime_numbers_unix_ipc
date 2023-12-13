43b88022d49d589965d412529a50d77eaf0ae2e7
parkchamchi <parkchamchi@gmail.com>
2023-12-13 14:30:20 +0900

    cmd arg on `prime_client.c`

--- prime_common.h
+++ prime_common.h
@@ -9,6 +9,9 @@ typedef unsigned long long primenum_t;
 #define ARGSIZE 3
 #define PBUFSIZE (sizeof (struct primemsgbuf) - sizeof (long))
 
+//EMPTY: "not set, unknown", ALLOC: "allocated by other process, or unavailable"
+enum Numstat { EMPTY = 00, PRIME = 01, NOTPRIME = 02, ALLOC = 03 };
+
 enum PrimeCommands { CMD_ALLOC_ONE, CMD_SET_ONE, CMD_CHECK_NUM };
 enum PrimeMtypes { MTYP_REQ = 1, MTYP_RES };
 
