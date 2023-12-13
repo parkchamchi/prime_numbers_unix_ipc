2a0141caeb2f569fc1f8d65bb52b4a2849a99465
kojunsik <127278960+kojunsik@users.noreply.github.com>
2023-12-13 15:24:00 +0900

    Add files via upload

--- prime_server.c
+++ prime_server.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>  
 
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -10,52 +9,50 @@
 #include "prime_files.h"
 
 void signal_handler(int signal) {
-    if (signal == SIGINT) {
-        printf("\nCtrl+C pressed. Cleaning up...\n");
-        cleanup();
-        exit(0);
-    }
+		if (signal == SIGINT) {
+        	printf("\nCtrl+C pressed. Cleaning up...\n");
+        	cleanup();
+        	exit(0);
+    	}
 }
 
 
 int main(void) {
-    struct primemsgbuf inmsg = { 0 };
-    key_t key;
-    int msgid;
+		struct primemsgbuf inmsg = { 0 };
+		key_t key;
+		int msgid;
 
-    int len;
+		int len;
 
-    signal(SIGINT, signal_handler);
+		key = ftok(KEYPATH, PROJID);
+		if ((msgid = msgget(key, IPC_CREAT | 0644)) < 0) {
+			perror("msgget");
+			exit(1);
+		}
 
-    key = ftok(KEYPATH, PROJID);
-    if ((msgid = msgget(key, 0)) < 0) {
-        perror("msgget");
-        exit(1);
-    }
+		startup();
 
-    startup();
+	/* LOOP */
+		while (1) {
+			len = msgrcv(msgid, &inmsg, PBUFSIZE, 0, 0); //msgtype, msgflag
 
-    /* LOOP */
-    while (1) {
-        len = msgrcv(msgid, &inmsg, sizeof (inmsg), 0, 0); //msgtype, msgflag
+			for (int i = 0; i < ARGSIZE; i++)
+				printf("%lld ", inmsg.args[i]);
+			puts("");
 
-        for (int i = 0; i < ARGSIZE; i++)
-            printf("%lld ", inmsg.args[i]);
-        puts("");
+			if (inmsg.cmd == CMD_CHECK_NUM) {
+				puts("CHECK_NUM");
+				enum Numstat res = check_num(inmsg.args[0]);
 
-        if (inmsg.cmd == CMD_CHECK_NUM) {
-            puts("CHECK_NUM");
-            enum Numstat res = check_num(inmsg.args[0]);
+			//ret.
+				struct primemsgbuf outmsg = { MTYP_RES, inmsg.cmd, { res } };
+				if (msgsnd(msgid, (void *) &outmsg, PBUFSIZE, IPC_NOWAIT) == -1) {
+					perror("msgsnd");
+					exit(1);
+				}
+			}
+		}
 
-            //ret.
-            struct primemsgbuf outmsg = { MTYP_RES, inmsg.cmd, { res } };
-            if (msgsnd(msgid, (void *) &outmsg, sizeof (outmsg), IPC_NOWAIT) == -1) {
-                perror("msgsnd");
-                exit(1);
-            }
-        }
-    }
-
-    cleanup(); //TODO: when interrupted or killed, make sure this is activated using signal catching
-    return 0;
+		cleanup(); //TODO: when interrupted or killed, make sure this is activated using signal catching
+		return 0;
 }
